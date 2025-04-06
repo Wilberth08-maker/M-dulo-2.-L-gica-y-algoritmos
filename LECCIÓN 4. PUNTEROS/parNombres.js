910ef07835206cc3c0b8e6fb841191599d5dd4fb
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-04-06 17:05:31 -0500

    Se añadio la solución al ejercicio de la lección 4 de campus

---
+++ LECCIÓN 4. PUNTEROS/parNombres.js
@@ -0,0 +1,30 @@
+// console.log('Hola desde JS');
+
+
+const invitados = ["Ana", "Carlos", "Cecilia", "Daniel", "Diana", "Eduardo"];
+
+function encontrarPareja(arr) {
+    let inicio = 0;
+    let siguiente = 1;
+
+    while (siguiente < arr.length) {
+
+        // Con map:
+        const iniciales = invitados.map(inciales => inciales[0]);
+
+        if(iniciales[inicio] === iniciales[siguiente]){
+
+            return [invitados[inicio], invitados[siguiente]];        
+        }
+
+        // Avanzar punteros si no coinciden
+        inicio ++;
+        siguiente++;
+        
+    }
+
+    return null; // Si no se encuentra ningún par
+}
+
+console.log(encontrarPareja(invitados));
+// Resultado: ['Carlos', 'Cecilia']
