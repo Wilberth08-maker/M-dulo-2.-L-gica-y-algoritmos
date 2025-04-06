273e1c9a0794f65217c37dfc59aa76f6cd174359
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-04-06 13:58:22 -0500

    Se agregó una modificación de sintaxis en usuario.js, git

--- LECCIÓN 1/usuario.js
+++ LECCIÓN 1/usuario.js
@@ -7,7 +7,7 @@ import { registrarDestino, mostrarItinerario } from "./viajes.js";
 const iniciarApp = () => {
     // Ejemplo de cómo registrar destinos
     registrarDestino("Paris", "2025-06-15", "Avión");
-    registrarDestino("Londres", "2025-07-01", "Tren");
+    registrarDestino("Londres", "2025-07-01", "Tren"); 
     registrarDestino("Zermatt", "2025-08-08", "Avión");
     registrarDestino("Mérida", "2025-09-14", "Camión");
 
